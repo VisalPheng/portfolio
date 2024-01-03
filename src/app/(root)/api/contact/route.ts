537a6f00085a2e0a5451581a13f5b0e6bfb7c46a
VisalPheng <phengvisall@gmail.com>
2024-01-03 19:57:40 +0700

    fix api key

--- src/app/(root)/api/contact/route.ts
+++ src/app/(root)/api/contact/route.ts
@@ -1,9 +1,8 @@
 import { title } from "@/constants/strings";
-import ContactEmailTemplate from "@/shared/components/other/email-template";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_KEY);
+// const resend = new Resend(process.env.RESEND_KEY);
 
 export async function POST(request: Request) {
   try {
@@ -12,31 +11,15 @@ export async function POST(request: Request) {
     if (!name || !email || !message || !recaptcha_token)
       return NextResponse.json({ error: "Invalid data, please try again!" });
 
-    const isValidCaptcha = await verifyCaptcha(recaptcha_token);
-    if (!isValidCaptcha)
-      return NextResponse.json({ error: "Invalid captcha, please try again!" });
-    data.recaptcha_token = undefined;
-
-    const res = await resend.emails.send({
-      subject: "Thank you for contacting me",
-      from: title + "<" + process.env.SENDER_EMAIL + ">",
-      to: [email],
-      bcc: process.env.FORWARD_EMAIL,
-      react: ContactEmailTemplate(data),
-    });
-    return NextResponse.json(res);
+    // const res = await resend.emails.send({
+    //   subject: "Thank you for contacting me",
+    //   from: title + "<" + process.env.SENDER_EMAIL + ">",
+    //   to: [email],
+    //   bcc: process.env.FORWARD_EMAIL,
+    //   react: ContactEmailTemplate(data),
+    // });
+    // return NextResponse.json(res);
   } catch (error) {
     return NextResponse.json({ error });
   }
 }
-
-const verifyCaptcha = async (token: string) => {
-  const response = await fetch(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
-    {
-      method: "POST",
-    }
-  );
-  const data = await response.json();
-  return data.success;
-};
